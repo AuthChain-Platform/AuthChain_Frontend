25f8775404e4bc9b956e5a6a48bff2d39d3d901a
rozzy <rosekaremeri@gmail.com>
2024-11-13 14:10:13 +0300

    Feature: set up footer

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ import LandingPageii from "./components/LandingPageii";
 const page = () => {
   return (
     <div>
-        <h1 className="text-4xl font-bold text-gray-900">My Heading</h1>
+        <h1 className="text-4xl font-bold text-gray-900">Landing Page Section 1</h1>
         <p className="text-lg text-gray-700">Some descriptive text here.</p>
       <Button className="bg-[#2711F1] text-white rounded-[20px]">Default Button</Button>
       <LandingPageii />
