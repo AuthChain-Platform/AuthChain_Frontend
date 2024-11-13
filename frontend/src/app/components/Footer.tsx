b6c54736aae3c265d793c853db1ed032119c8952
rozzy <rosekaremeri@gmail.com>
2024-11-13 14:20:19 +0300

    Feature: footer

--- frontend/src/app/components/Footer.tsx
+++ frontend/src/app/components/Footer.tsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
     <div>
-<div className='flex flex-row'>
-<div>
-    
-</div>
-</div>
-
+      <div className="flex flex-row">
+        <div className="flex flex-col">
+          <img src="/ftlogo.png" alt="footer logo" />
+          <p>
+            Authentic Chain addresses the critical need for reliable product
+            authentication in the global market
+          </p>
+        </div>
+        <div className="flex flex-row">
+          <div className="flex flex-col">
+            <p className="text-lg">COMPANY</p>
+            <p>About</p>
+            <p>Contacts</p>
+            <p>FAQs</p>
+          </div>
+          <div className="flex flex-col">
+            <p className="text-lg">LEGAL</p>
+            <p>Terms of Service</p>
+            <p>Privacy Policy</p>
+          </div>
+          <div className="flex flex-col">
+            <p className="text-lg">EXPLORE</p>
+            <p>Platform</p>
+            <p>Solutions</p>
+            <p>Manufacturer</p>
+            <p>Retailer</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
