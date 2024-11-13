23f4b82ec181426d128ce4f129e022d6ff6205eb
rozzy <rosekaremeri@gmail.com>
2024-11-13 13:11:29 +0300

    Feature: add landing page sections

--- frontend/src/app/components/LandingPageii.tsx
+++ frontend/src/app/components/LandingPageii.tsx
@@ -18,54 +18,68 @@ const LandingPageii = () => {
         </p>
       </div>
 
-      {/* Feature Section */}
 
-      <div className="flex flex-col lg:flex-row items-center lg:items-stretch space-x-10 p-4">
-  <img
-    src="/group.png"
-    alt="Combat Counterfeiting"
-    className="w-full lg:w-1/3 xl:w-1/2 h-auto lg:h-full object-cover rounded-lg mb-4 lg:mb-0"
-  />
-  <div className="flex flex-col text-left h-full">
-    <p className="text-4xl text-[#2711F1] font-light mb-7">
-      Combat Counterfeiting
-    </p>
-    <p className="text-2xl text-gray-700 w-full lg:w-[90%] xl:w-[80%] mb-14">
-      Implementing tamper-proof identification and creating a verifiable
-      product history, we enable instant authenticity verification for
-      secure and transparent product tracking. This ensures
-      .....................
-    </p>
-    <Button className="bg-[#2711F1] text-white rounded-[20px] w-full lg:w-[20%] xl:w-[15%]">
-      Read More
-    </Button>
-  </div>
-</div>
+      <div className="flex flex-col lg:flex-row items-center lg:items-stretch space-x-7 p-4 mb-5">
+        <img
+          src="/counterfeit.png"
+          alt="Combat Counterfeiting"
+          className="w-full lg:w-1/3 xl:w-1/2 h-auto lg:h-full object-cover rounded-lg mb-4 lg:mb-0"
+        />
+        <div className="flex flex-col text-left h-full">
+          <p className="text-3xl text-[#2711F1] font-light mb-7">
+            Combat Counterfeiting
+          </p>
+          <p className="text-xl text-gray-700 w-full lg:w-[90%] xl:w-[80%] mb-14">
+            Implementing tamper-proof identification and creating a verifiable
+            product history, we enable instant authenticity verification for
+            secure and transparent product tracking. This ensures
+            .....................
+          </p>
+          <Button className="bg-[#2711F1] text-white rounded-[20px] w-full lg:w-[20%] xl:w-[15%]">
+            Read More
+          </Button>
+        </div>
+      </div>
 
+      <div className="flex flex-col lg:flex-row items-center lg:items-stretch space-x-7 p-4">
+        <div className="flex flex-col text-left h-full">
+          <p className="text-3xl text-[#2711F1] font-light mb-7">
+            Enhance Supply Chain Transparency
+          </p>
+          <p className="text-xl text-gray-700 w-full lg:w-[90%] xl:w-[80%] mb-14">
+            Track the complete journey of each product from manufacture to
+            consumer, recording every ownership transfer and monitoring
+            conditions during.......................
+          </p>
+          <Button className="bg-[#2711F1] text-white rounded-[20px] w-full lg:w-[20%] xl:w-[15%]">
+            Read More
+          </Button>
+        </div>
+        <img
+          src="/supply_chain.png"
+          alt="Combat Counterfeiting"
+          className="w-full lg:w-1/3 xl:w-1/2 h-auto lg:h-full object-cover rounded-lg mb-4 lg:mb-0"
+        />
+      </div>
 
-
-<div className="flex flex-col lg:flex-row items-center lg:items-stretch space-x-10 p-4">
-
-  <div className="flex flex-col text-left h-full">
-    <p className="text-4xl text-[#2711F1] font-light mb-7">
-      Combat Counterfeiting
-    </p>
-    <p className="text-2xl text-gray-700 w-full lg:w-[90%] xl:w-[80%] mb-14">
-      Implementing tamper-proof identification and creating a verifiable
-      product history, we enable instant authenticity verification for
-      secure and transparent product tracking. This ensures
-      .....................
-    </p>
-    <Button className="bg-[#2711F1] text-white rounded-[20px] w-full lg:w-[20%] xl:w-[15%]">
-      Read More
-    </Button>
-  </div>
-  <img
-    src="/group.png"
-    alt="Combat Counterfeiting"
-    className="w-full lg:w-1/3 xl:w-1/2 h-auto lg:h-full object-cover rounded-lg mb-4 lg:mb-0"
-  />
-</div>
+      <div className="flex flex-col lg:flex-row items-center lg:items-stretch space-x-7 p-4 mb-5">
+        <img
+           src="/supply_chain.png"
+          alt="Combat Counterfeiting"
+          className="w-full lg:w-1/3 xl:w-1/2 h-auto lg:h-full object-cover rounded-lg mb-4 lg:mb-0"
+        />
+        <div className="flex flex-col text-left h-full">
+          <p className="text-3xl text-[#2711F1] font-light mb-7">
+           Improve User Experience
+          </p>
+          <p className="text-xl text-gray-700 w-full lg:w-[90%] xl:w-[80%] mb-14">
+          Offer a simple, mobile-first authentication process with rapid response times for seamless product verification. This approach ensures that users can quickly confirm authenticity  ....................
+          </p>
+          <Button className="bg-[#2711F1] text-white rounded-[20px] w-full lg:w-[20%] xl:w-[15%]">
+            Read More
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
