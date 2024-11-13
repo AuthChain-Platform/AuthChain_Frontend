eea7180d53d1d90a5186ce5a17065aa4b67ce504
rozzy <rosekaremeri@gmail.com>
2024-11-13 14:15:04 +0300

    Fix: Footer structure

--- frontend/src/app/components/Footer.tsx
+++ frontend/src/app/components/Footer.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+<div className='flex flex-row'>
+<div>
+    
+</div>
+</div>
+
+    </div>
   )
 }
 
