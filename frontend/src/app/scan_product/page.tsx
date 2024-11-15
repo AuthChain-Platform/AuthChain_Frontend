34e4caad0a17bc5afb835e308afdc824766bb021
rozzy <rosekaremeri@gmail.com>
2024-11-15 15:58:55 +0300

    Feat: scan product page

---
+++ frontend/src/app/scan_product/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+<div className='flex flex-row'>
+<div className='flex flex-col space-y-5'>
+    <p className='text-2xl font-semibold'>Verify Product</p>
+    <p className='text-l'>
+        Scan a product to verify its authenticity and track its journey through the supply chain
+    </p>
+</div>
+</div>
+
+    </div>
+  )
+}
+
+export default page
