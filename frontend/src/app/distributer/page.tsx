fd9c7fe814988fb02bf885317c7ae178b5ed675b
rozzy <rosekaremeri@gmail.com>
2024-11-18 21:28:44 +0300

    Feature: UI role definition

---
+++ frontend/src/app/distributer/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const distributer  = () => {
+  return (
+    <div>distributer set up</div>
+  )
+}
+
+export default distributer 
---
+++ frontend/src/app/manufacturer/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Manufacturer setup</div>
+  )
+}
+
+export default page
